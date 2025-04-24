ddae1308d7d6a5d975caf8ae66c3eb6a590b3ffe
Fernanda <fernandakagami@gmail.com>
2025-04-24 09:44:52 -0300

    Add http PUT

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c1b9a4d8962164cc0a49f1270684dd89d53873b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51e92764e70e56374b6740eeb40c6393c4df3e1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
