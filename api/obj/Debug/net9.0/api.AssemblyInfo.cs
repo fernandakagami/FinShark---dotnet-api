37e94cbd71100b26516999d1544a84cc34f2f93a
Fernanda <fernandakagami@gmail.com>
2025-04-28 07:48:34 -0300

    Add async/await functions

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddae1308d7d6a5d975caf8ae66c3eb6a590b3ffe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+382558db735766c85870ce00df4ce7d2b7dc967f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
